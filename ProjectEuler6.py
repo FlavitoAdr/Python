e48424580dae7c5850f26b507d1c72774a291df2
FlavitoAdr <67709192+FlavitoAdr@users.noreply.github.com>
2021-04-23 03:11:53 -0300

    Create ProjectEuler6.py

---
+++ ProjectEuler6.py
@@ -0,0 +1,9 @@
+#Desafio 6#
+potencia1 = 0
+for i in range(1, 101):
+  potencia1 += i**2
+potencia2 = int((1+100)/2*100)**2
+resultado = potencia2 - potencia1
+print(resultado)
+
+#Resultado 25164150
